cbfc64e4c71dd9cda83648feda759be016a0d880
Mrebb <madhu.rebbana@gmail.com>
2018-03-23 00:45:04 -0700

    insert Dash kata

--- replace-all-dots-/solution.js
+++
@@ -1,4 +0,0 @@
-    var replaceDots = function(str) {
-        // replace function helps to replace the found pattern passed as first parameter with the second parameter
-        return str.replace(/\./g, '-');
-    }
